fa702ccc7fe69246b4d75b4703bcff162da4d9e2
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2016-01-10 01:25:31 -0500

    factory added

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var app = angular.module("sportsApp", []);
 
+
+app.factory("posts", [function(){
+  // Create an object with an array of posts
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
 app.controller("MainController", [
+  // Service injection to the main controller
   "$scope",
-  function($scope){
+  "posts",
+  function($scope, posts){
     $scope.test = "Hello, world!";
-    $scope.posts = [
-      {title: "post 1", upvotes: 5},
-      {title: "post 2", upvotes: 10}
-    ];
+
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function(){
+      // This prevents the user from adding a blank entry
+      if(!$scope.title || $scope.title === "") {return;}
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title="";
+      $scope.link="";
+    };
+
+    $scope.addUpvote = function(post){
+      post.upvotes += 1;
+    };
+
   }
 ]);
