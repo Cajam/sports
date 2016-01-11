7a095780127823e8dcebce2bd8de9efb6662de3c
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2016-01-11 15:16:07 -0500

    fixing the ui

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-var app = angular.module("sportsApp", []);
+var app = angular.module("sportsApp", ["ui.router"]);
+
+app.config([
+  "$stateProvider",
+  "$urlRouterProvider",
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/home",
+        templateUrl: "/home.html",
+        controller: "MainController"
+      });
+
+      $urlRouterProvider.otherwise("home");
+  }]);
 
 
 app.factory("posts", [function(){
