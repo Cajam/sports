3691ffba498810032350889bc002700d86d0764c
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2016-01-11 15:03:53 -0500

    Revert "Finished adding comments"
    This reverts commit 910c89ee28677c8f7ca3ca14f2aa2037e432f2ec.

--- app.js
+++ app.js
@@ -1,31 +1,11 @@
-var app = angular.module("sportsApp", ["ui.router"]);
+var app = angular.module("sportsApp", []);
 
-app.config([
-  "$stateProvider",
-  "$urlRouterProvider",
-
-  function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state("home", {
-        url: "/home",
-        templateUrl: "/home.html",
-        controller: "MainController"
-      })
-      .state("posts", {
-        url: "/posts/{id}",
-        templateUrl: "/posts.html",
-        controller: "PostsController"
-      });
-    // Undefined URLs will go to home using .otherwise
-    $urlRouterProvider.otherwise("home");
-  }]);
 
 app.factory("posts", [function(){
   // Create an object with an array of posts
   var o = {
     posts: []
   };
-  // Return the object so it becomes available to other services for injection
   return o;
 }]);
 
@@ -44,11 +24,7 @@ app.controller("MainController", [
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: "Joe", body: "Kobe kobe", upvotes: 0},
-          {author: "Bobby", body: "Drake drake I hate the Heat", upvotes: 0}
-        ]
+        upvotes: 0
       });
       $scope.title="";
       $scope.link="";
@@ -60,20 +36,3 @@ app.controller("MainController", [
 
   }
 ]);
-
-  app.controller("PostsController", [
-    "$scope",
-    "$stateParams",
-    "posts",
-    function($scope, $stateParams, posts){
-      $scope.post = posts.posts[$stateParams.id];
-      $scope.addComment = function(){
-        if($scope.body === "") { return; }
-        $scope.post.comments.push({
-          body: $scope.body,
-          author: "user",
-          upvotes: 0
-        });
-        $scope.body = "";
-      };
-    }]);
