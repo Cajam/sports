145b5f8bdfd05f4cea772b55c3446d83ede39098
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2016-01-13 00:14:18 -0500

    finished node

--- sports/public/javascripts/angularApp.js
+++ sports/public/javascripts/angularApp.js
@@ -25,13 +25,85 @@ app.config([
             return posts.get($stateParams.id);
           }]
         }
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: '/login.html',
+        controller: 'AuthController',
+        onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+          }
+        }]
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: '/register.html',
+        controller: 'AuthController',
+        onEnter: ['$state', 'auth', function($state, auth){
+          if(auth.isLoggedIn()){
+            $state.go('home');
+          }
+        }]
       });
 
       $urlRouterProvider.otherwise("home");
   }]);
 
+app.factory("auth", ["$http", "$window", function($http, $window) {
+  var auth = {};
 
-app.factory("posts", ["$http", function($http){
+  auth.saveToken = function(token) {
+    $window.localStorage["sports-token"] = token;
+  }
+
+  auth.getToken = function() {
+    return $window.localStorage["sports-token"];
+  }
+
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if (token) {
+      var payload = JSON.parse($window.atob(token.split(".")[1]));
+
+      return payload.exp = Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if (auth.isLoggedIn()) {
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split(".")[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user) {
+    return $http.post("/register", user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user) {
+    return $http.post("/login", user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function() {
+    $window.localStorage.removeItem("sports-token");
+  };
+
+
+  return auth;
+}]);
+
+
+app.factory("posts", ["$http", "auth", function($http, auth){
   // Create an object with an array of posts
   var o = {
     posts: []
@@ -50,26 +122,35 @@ app.factory("posts", ["$http", function($http){
   };
   // Save new posts to make them persistent with a post function
   o.create = function(post) {
-    return $http.post("/posts", post).success(function(data) {
+    return $http.post("/posts", post, {
+      headers: {Authorization: "Bearer " +auth.getToken()}
+    }).success(function(data) {
       o.posts.push(data);
     });
   };
 
   o.upvote = function(post) {
-    return $http.put("/posts/" + post._id + "/upvote").success(function(data) {
+    return $http.put("/posts/" + post._id + "/upvote", null, {
+      headers: {Authorization: "Bearer " +auth.getToken()}
+    }).success(function(data) {
       post.upvotes += 1;
     });
   };
 
   o.addComment = function(id, comment) {
-    return $http.post("/posts/" + id + "/comments", comment);
+    return $http.post("/posts/" + id + "/comments", comment, {
+      headers: {Authorization: "Bearer " +auth.getToken()}
+    });
   };
 
   o.upvoteComment = function(post, comment) {
-    return $http.put("/posts/" + post._id + "/comments/" + comment._id + "/upvote").success(function(data){
+    return $http.put("/posts/" + post._id + "/comments/" + comment._id + "/upvote", null, {
+      headers: {Authorization: "Bearer " +auth.getToken()}
+    }).success(function(data){
       comment.upvotes += 1
     });
   };
+
   return o;
 }]);
 
@@ -106,6 +187,7 @@ app.controller("PostsController", [
   "post",
   function($scope, $stateParams, posts, post){
     $scope.post = post;
+    $scope.isLoggedIn = auth.isLoggedIn;
     $scope.addComment = function(){
       if($scope.body === "") {return;}
         posts.addComment(post._id, {
@@ -122,3 +204,36 @@ app.controller("PostsController", [
       posts.upvoteComment(post, comment);
     }
   }]);
+
+app.controller('AuthController', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}]);
+
+app.controller('NavController', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
